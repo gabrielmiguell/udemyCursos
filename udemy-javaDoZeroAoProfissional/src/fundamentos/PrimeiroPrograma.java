fd4178b7869c37192314a74f5fd849bd3f05b3ef
Gabriel Miguel <gabrieln.miguel@hotmail.com>
2022-07-25 01:17:54 -0300

    Adicionando algumas linhas de código no meu primeiro programa em Java

--- udemy-javaDoZeroAoProfissional/src/fundamentos/PrimeiroPrograma.java
+++ udemy-javaDoZeroAoProfissional/src/fundamentos/PrimeiroPrograma.java
@@ -2,7 +2,11 @@ package fundamentos;
 
 public class PrimeiroPrograma {
 	public static void main(String[] args) {
-		System.out.println("Primeiro Programa!!!");
+		System.out.println("Primeiro Programa - Parte #001");
+		System.out.println("Primeiro Programa - Parte #002");
+		System.out.println("Primeiro Programa - Parte #003");
+		System.out.println("Primeiro Programa - Parte #004");
+		System.out.println("Primeiro Programa - Parte #005");
 		
 	}
 
